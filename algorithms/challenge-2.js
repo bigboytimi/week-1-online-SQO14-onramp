50505877a236fabee20e33cd9dc4ac77ffceecbe
Timi O <olutimiolowookere@icloud.com>
2022-10-30 11:35:01 +0100

    second challenge

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,12 @@
-function fizzBuzz(input){
-
-  
+function fizzBuzz(input) {
+  if (input % 3 === 0 && input % 5 === 0) {
+    return "fizzbuzz";
+  } else if (input % 5 === 0) {
+    return "buzz";
+  } else if (input % 3 === 0) {
+    return "fizz";
+  } else {
+    return input;
+  }
 }
-
-
-module.exports = fizzBuzz
+module.exports = fizzBuzz;
