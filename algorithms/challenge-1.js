a30953681aa1e2bae207bb11704b104d000fe66f
Timi O <olutimiolowookere@icloud.com>
2022-10-30 11:28:24 +0100

    first challenge solved

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,12 @@
 function countTruthy(arr) {
-
+  let i = 0;
+  let count = 0;
+  while (i < arr.length) {
+    if (arr[i] === true) {
+      count++;
+    }
+    i++;
+  }
+  return count;
 }
-module.exports = countTruthy
+module.exports = countTruthy;
