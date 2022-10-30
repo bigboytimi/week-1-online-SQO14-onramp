0c8d462803826fed178a21a7fe4a8a0df4ec4312
Timi O <olutimiolowookere@icloud.com>
2022-10-30 15:18:52 +0100

    fourth challenge

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,27 @@
 function calculateGrade(marks) {
- 
- }
-  module.exports =calculateGrade
+  let i;
+  let count = 0;
+  let sum = 0;
+  let average;
+
+  for (i = 0; i < marks.length; i++) {
+    sum += marks[i];
+    count++;
+  }
+  average = sum / count;
+
+  if (average > 1 && average < 50) {
+    return "F";
+  } else if (average < 60) {
+    return "E";
+  } else if (average < 70) {
+    return "D";
+  } else if (average < 80) {
+    return "C";
+  } else if (average < 90) {
+    return "B";
+  } else if (average <= 100) {
+    return "A";
+  }
+}
+module.exports = calculateGrade;
